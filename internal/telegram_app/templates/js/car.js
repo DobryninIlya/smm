3a56a345df862cc956c071513bf72b023d30d911
Ilya Dobrynin <mr.woodysimpson@gmail.com>
2024-01-11 15:02:25 +0300

    t

--- internal/telegram_app/templates/js/car.js
+++ internal/telegram_app/templates/js/car.js
@@ -1,8 +1,9 @@
 let tg = window.Telegram.WebApp;
 let text = "RENT";
 
+alert(tg.initDataUnsafe.user.username);
 tg.MainButton.setText(text);
 tg.MainButton.show();
-tg.MainButton.enable()
+tg.MainButton.enable();
 
 // tg.MainButton.requestContact()
